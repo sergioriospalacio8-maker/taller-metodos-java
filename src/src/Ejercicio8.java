78f04ba39792583ae54691dc0f213bd508212d10
Sergio <[sergioriospalacio8gmail.com]git commit -m 2Primer commit>
2026-08-19 12:02:18 -0500

    feat: ejercicio 8 - Metodo que retorna un arreglo - realize la primera parte del ejercicio

--- src/src/Ejercicio8.java
+++ src/src/Ejercicio8.java
@@ -1,2 +1,11 @@
 public class Ejercicio8 {
+    public static void main(String[] args){
+
+        int[] tabla = generarTablaMultiplicar(5);
+        System.out.println("Tabla del 5:");
+
+        for (int i = 0; i < tabla.length; i++){
+            System.out.println("5 x" + (i + 1) + "=" + tabla[i]);
+        }
+    }
 }
