846517e7791993f768e98a246d5f416d642fd863
Sergio <[sergioriospalacio8gmail.com]git commit -m 2Primer commit>
2026-08-19 11:36:36 -0500

    feat: ejercicio 7 - Metodo con arreglo con parametro - realize la primera parte del ejercicio

--- src/src/Ejercicio7.java
+++ src/src/Ejercicio7.java
@@ -1,2 +1,10 @@
 public class Ejercicio7 {
+    public static void main(String[] args){
+
+        double[] calificaciones = {4.0 , 3.5, 5.0, 4.5, 3.0,};
+        double promedio = calcularPromedioClase(calificaciones);
+        System.out.println("El promedio general de la clase es:" + promedio);
+    }
+
+    
 }
