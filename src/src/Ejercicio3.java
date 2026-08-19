91662cc1a35aacd7305df223c8f5edc9a53acf70
Sergio <[sergioriospalacio8gmail.com]git commit -m 2Primer commit>
2026-08-19 10:13:27 -0500

    feat: ejercicio 3 - Realize la segunda y ultima parte del ejercicio

--- src/src/Ejercicio3.java
+++ src/src/Ejercicio3.java
@@ -3,4 +3,8 @@ public class Ejercicio3 {
         double area = calcularAreaRectangulo(5, 3);
         System.out.println("El area del rectangulo es:" + area);
     }
+
+    public static double calcularAreaRectangulo(double base, double altura){
+        return base * altura;
+    }
 }
