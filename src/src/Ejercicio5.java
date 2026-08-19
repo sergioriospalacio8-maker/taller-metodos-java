47bcc2ada3a2993f97d0b2d80a324d1879c50803
Sergio <[sergioriospalacio8gmail.com]git commit -m 2Primer commit>
2026-08-19 10:45:56 -0500

    feat: ejercicio 5 - realize la segunda version de calcularPromedio y la ultima parte del ejercicio

--- src/src/Ejercicio5.java
+++ src/src/Ejercicio5.java
@@ -12,4 +12,13 @@ public class Ejercicio5 {
     public static double calcularPromedio(double nota1, double nota2){
         return (nota1 + nota2) / 2;
     }
+
+    public static double calcularPromedio(double[]notas){
+        double suma = 0;
+
+        for (int i = 0; i < notas.length; i++){
+            suma += notas[i];
+        }
+        return suma / notas.length;
+    }
 }
