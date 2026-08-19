4cc08ff0e4c2feab8e898c6426f35885162993bb
Sergio <[sergioriospalacio8gmail.com]git commit -m 2Primer commit>
2026-08-19 10:35:09 -0500

    feat: ejercicio 5 - Sobrecarga de metodos - realize la primera parte del ejercicio

--- src/src/Ejercicio5.java
+++ src/src/Ejercicio5.java
@@ -1,2 +1,11 @@
 public class Ejercicio5 {
+    public static void main(String[] args){
+
+        double promedio1 = calcularPromedio(4.0, 5.0);
+        System.out.println("Promedio de dos notas:" + promedio1);
+        double[] notas = {4.0 , 3.5, 5.0, 4.5};
+        double promedio2 = calcularPromedio(notas);
+
+        System.out.println("Promedio del arreglo:" + promedio2);
+    }
 }
