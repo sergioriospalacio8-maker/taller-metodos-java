357ee592676cab434f71b8e94c1c3dba85e9b831
Sergio <[sergioriospalacio8gmail.com]git commit -m 2Primer commit>
2026-08-19 12:14:43 -0500

    feat: ejercicio 9 - Logica condicional - realize la primera parte del ejercicio

--- src/src/Ejercicio9.java
+++ src/src/Ejercicio9.java
@@ -1,2 +1,7 @@
 public class Ejercicio9 {
+    public static void main(String[] args){
+
+        String resultado = clasificarNota(4.7);
+        System.out.println("Resultado:" + resultado);
+    }
 }
