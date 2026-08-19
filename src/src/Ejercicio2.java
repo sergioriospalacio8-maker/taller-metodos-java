92e7948d7821a70423106e23e9c7978444b950ab
Sergio <[sergioriospalacio8gmail.com]git commit -m 2Primer commit>
2026-08-19 10:04:19 -0500

    feat: realizamos el ejercicio 2 - metodo void con parametros

--- src/src/Ejercicio2.java
+++ src/src/Ejercicio2.java
@@ -2,4 +2,8 @@ public class Ejercicio2 {
     public static void main(String[] args){
         saludarEstudiante("Sergio", 20);
     }
+
+    public static void saludarEstudiante(String nombre, int edad){
+        System.out.println("Hola "+ nombre + ", tienes" + edad + "años. ¡Bienvenido!");
+    }
 }
