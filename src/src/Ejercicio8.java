f1911839ae51ba9406d4425027330346af9c56c6
Sergio <[sergioriospalacio8gmail.com]git commit -m 2Primer commit>
2026-08-19 12:07:37 -0500

    feat: ejercicio 8 - realize la segunda y ultima parte del ejercicio

--- src/src/Ejercicio8.java
+++ src/src/Ejercicio8.java
@@ -8,4 +8,14 @@ public class Ejercicio8 {
             System.out.println("5 x" + (i + 1) + "=" + tabla[i]);
         }
     }
+
+    public static int[] generarTablaMultiplicar(int numero){
+        int[] resultados = new int[10];
+
+        for (int i = 0; i < resultados.length; i++){
+            resultados[i] = numero * (i + 1);
+        }
+
+        return resultados;
+    }
 }
