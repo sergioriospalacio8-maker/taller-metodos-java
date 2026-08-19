798cb390bab75b63887ffe21754cd2e5eec2dfe3
Sergio <[sergioriospalacio8gmail.com]git commit -m 2Primer commit>
2026-08-19 10:59:47 -0500

    feat: ejercicio 6 - Static vs instancia - realize la primera parte del ejercicio

--- src/src/Ejercicio6.java
+++ src/src/Ejercicio6.java
@@ -1,2 +1,14 @@
 public class Ejercicio6 {
+    public static void main(String[] args){
+
+        // Creamos un objeto de la clase Estudiante
+        Estudiante estudiante1 = new Estudiante(4.5);
+
+        // Llamo al metodo de instancia
+        estudiante1.mostrarBoeltin();
+
+        // Llamo al metodo satic sin crear un objeto
+        double resultado = Estudiante.compararNotas(4.5, 3.8);
+        System.out.println("La diferencia entre las notas es:" + resultado);
+    }
 }
