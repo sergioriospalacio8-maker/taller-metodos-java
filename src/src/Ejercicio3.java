97b29780b25d94f734b53f480cae9eb587684dc1
Sergio <[sergioriospalacio8gmail.com]git commit -m 2Primer commit>
2026-08-19 10:10:29 -0500

    feat: ejercicio 3 - realize la primera parte del ejercicio y el metodo main

--- src/src/Ejercicio3.java
+++ src/src/Ejercicio3.java
@@ -1,2 +1,6 @@
 public class Ejercicio3 {
+    public static void main(String[] args){
+        double area = calcularAreaRectangulo(5, 3);
+        System.out.println("El area del rectangulo es:" + area);
+    }
 }
