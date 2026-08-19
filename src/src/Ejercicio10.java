53a2b4e54ab2deedd3e5811f81308da89a2fb508
Sergio <[sergioriospalacio8gmail.com]git commit -m 2Primer commit>
2026-08-19 12:47:31 -0500

    feat: ejercicio 10 - ejercicio integrado - realize la primera parte del ejercicio

--- src/src/Ejercicio10.java
+++ src/src/Ejercicio10.java
@@ -1,2 +1,15 @@
 public class Ejercicio10 {
+    public static void main(String[] args){
+
+        Inventario inventario = new Inventario();
+
+        inventario.agregarProductos(new Producto("Arroz", 4500, 10));
+        inventario.agregarProductos(new Producto("Leche", 3500, 8));
+        inventario.agregarProductos(new Producto("Pan", 2000, 15));
+
+        Inventario.mostrarInventario(inventario.productos);
+        double total = Inventario.calcularValorTotalInventario(inventario.productos);
+        System.out.println("\nValor total del inventario: $" + total);
+        
+    }
 }
