88895929e41366b6922522440070285b70f25aa8
Sergio <[sergioriospalacio8gmail.com]git commit -m 2Primer commit>
2026-08-19 10:20:50 -0500

    feat: ejercicio 4 - Metodo con retorno booleano - realizamos la primera parte del ejercicio

--- src/src/Ejercicio4.java
+++ src/src/Ejercicio4.java
@@ -1,2 +1,6 @@
 public class Ejercicio4 {
+    public static void main(String[] args){
+        boolean resultado = esPar(8);
+        System.out.println("¿El numero es par?" + resultado);
+    }
 }
