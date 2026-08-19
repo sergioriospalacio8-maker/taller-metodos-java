efa5e194d3ea809648644524c440aed2ae9e2c25
Sergio <[sergioriospalacio8gmail.com]git commit -m 2Primer commit>
2026-08-19 12:20:10 -0500

    feat: ejercicio 8 - realize la segunda y ultima parte del ejercicio

--- src/src/Ejercicio9.java
+++ src/src/Ejercicio9.java
@@ -4,4 +4,17 @@ public class Ejercicio9 {
         String resultado = clasificarNota(4.7);
         System.out.println("Resultado:" + resultado);
     }
+
+    public static String clasificarNota(double nota){
+
+        if (nota < 3.0){
+            return "Reprobado";
+
+        } else if (nota < 4.5) {
+            return "Aprobado";
+
+        } else {
+            return "Excelente";
+        }
+    }
 }
