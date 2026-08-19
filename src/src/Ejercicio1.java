c8778819d515e871b9594f8d40acb76cdb56f8af
Sergio <[sergioriospalacio8gmail.com]git commit -m 2Primer commit>
2026-08-19 09:42:45 -0500

    feat: ejercicio 1 - crea metodo mostrarBienvenida

--- src/src/Ejercicio1.java
+++ src/src/Ejercicio1.java
@@ -1,5 +1,9 @@
 public class Ejercicio1 {
     public static void main(String[] args){
-        
+
+    }
+
+    public static void mostrarBienvenida(){
+        System.out.println("Bienvenido al instituto");
     }
 }
